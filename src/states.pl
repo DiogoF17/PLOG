f4086fe05c3e17dbff002de61d8bf30d58d6e9bf
Diogo Santos <diogofilipe1_santos@hotmail.com>
2020-11-03 19:11:32 +0000

    Arranjei e passei para estados

--- src/states.pl
+++ src/states.pl
@@ -4,17 +4,14 @@ SELECTS THE NEXT STATE FROM THE CURRENT STATE: MAINMENU
 
 next_state(mainMenu, Option) :-
     Option == 1,
-    initial(Board), save_board(Board),
     play.
 
 next_state(mainMenu, Option) :-
     Option == 2,
-    initial(Board), save_board(Board),
     play.
 
 next_state(mainMenu, Option) :-
     Option == 3,
-    initial(Board), save_board(Board),
     play.
 
 /* ----------------------------------------------
@@ -24,35 +21,29 @@ SELECTS THE NEXT STATE FROM THE CURRENT STATE: BOARD
    ----------------------------------------------
 */
 
-                /* All pieces were eliminated! */
-endOfGame:-
-    o_eliminated(O),
-    O =:= 10.
+/* All pieces were eliminated! */
+endOfGame(state(_, _, _, _, O_Eliminated, _)):-
+    O_Eliminated =:= 10.
 
-endOfGame:-
-    x_eliminated(X),
-    X =:= 10.
+endOfGame(state(_, _, _, X_Eliminated, _, _)):-
+    X_Eliminated =:= 10.
 
-endOfGame:-
-    z_eliminated(Z),
-    Z =:= 10.
+endOfGame(state(_, _, _, _, _, Z_Eliminated)):-
+    Z_Eliminated =:= 10.
 
-                /* All remaining pieces are touching the border of it's color */
+/* All remaining pieces are touching the border of it's color */
 
-endOfGame:-
-    o_eliminated(O),
-    Remaining is 10 - O,
-    count_O_touching_border(Num),
+endOfGame(state(Board, _, _, _, O_Eliminated, _)):-
+    Remaining is 10 - O_Eliminated,
+    count_O_touching_border(Board, Num),
     Remaining =:= Num.
 
-endOfGame:-
-    x_eliminated(X),
-    Remaining is 10 - X,
-    count_X_touching_border(Num),
+endOfGame(state(Board, _, _, X_Eliminated, _, _)):-
+    Remaining is 10 - X_Eliminated,
+    count_X_touching_border(Board, Num),
     Remaining =:= Num.
 
-endOfGame:-
-    z_eliminated(Z),
-    Remaining is 8 - Z,
-    count_Z_touching_border(Num),
+endOfGame(state(Board, _, _, _, _, Z_Eliminated)):-
+    Remaining is 8 - Z_Eliminated,
+    count_Z_touching_border(Board, Num),
     Remaining =:= Num.
